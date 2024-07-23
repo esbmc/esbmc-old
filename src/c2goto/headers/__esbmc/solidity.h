655a7fc1a2ae297ee954dd0ff6882a4b4b8eca55
prototypeC14 <caishiyu14luck@gmail.com>
2024-07-23 21:13:16 +0100

    fix link issue

--- src/c2goto/headers/__esbmc/solidity.h
+++ src/c2goto/headers/__esbmc/solidity.h
@@ -1,7 +1,8 @@
 /*
   The template/library for Solidity built-in variables, function and data structure
 */
-
+#ifndef SOLIDITY_H
+#define SOLIDITY_H
 
 /// header & typedef
 extern const char *sol_header;
@@ -26,5 +27,5 @@ extern const char *sol_mapping;
 /// external library
 extern const char *sol_itoa;
 
-
+#endif // SOLIDITY_H
 
