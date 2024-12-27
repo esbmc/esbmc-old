b4dba5e1317bc2b8f7c8f4606666998a41a7092b
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2024-12-27 14:50:02 +0000

    Update goto_symex.h

--- src/goto-symex/goto_symex.h
+++ src/goto-symex/goto_symex.h
@@ -928,7 +928,8 @@ protected:
     const std::string &property,
     const std::string &msg,
     const guardt &guard) override;
-
+  
+  void dereference_assume(const guardt &guard) override;
   void
   get_value_set(const expr2tc &expr, value_setst::valuest &value_set) override;
 
