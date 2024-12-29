a6cc8098b2a3e3c1dba49bf0e2f947ec4d6209f1
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2024-12-29 14:34:05 +0000

    Update symex_main.cpp

--- src/goto-symex/symex_main.cpp
+++ src/goto-symex/symex_main.cpp
@@ -1197,7 +1197,8 @@ void goto_symext::add_memory_leak_checks()
         return is_any ? is_any : gen_false_expr();
       };
   }
-
+  if (no_memory_cleanup_check)
+    return;
   for (auto const &it : dynamic_memory)
   {
     // Don't check memory leak if the object is automatically deallocated
@@ -1234,8 +1235,6 @@ void goto_symext::add_memory_leak_checks()
     // is described by a separate "allocation_guard".
     // (see "symex_mem" method in "goto-symex/builtin_functions.cpp").
     expr2tc cond = implies2tc(when, eq);
-    if (no_memory_cleanup_check)
-      cond = when;
     replace_dynamic_allocation(cond);
     cur_state->rename(cond);
     claim(
